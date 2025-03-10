5ba5e811653e02b77293d160b5512e67b5db344a
5unV <31982712+Cinvin@users.noreply.github.com>
2025-03-10 16:38:31 +0800

    chore: 增加提示。

--- ncmExtend/src/home/cloudImport.js
+++ ncmExtend/src/home/cloudImport.js
@@ -13,7 +13,8 @@ export const cloudImport= (uiArea) => {
                 'accept': 'application/json',
                 'aria-label': '选择文件'
             },
-            confirmButtonText: '导入'
+            confirmButtonText: '导入',
+            footer: '<div>请勿多个标签页同时操作，网易云有频率限制，若短时间内有大量请求，会请求失败。</div><div><a href="https://github.com/Cinvin/myuserscripts"  target="_blank"><img src="https://img.shields.io/github/stars/cinvin/myuserscripts?style=social" alt="Github"></a></div>',
         })
             .then((result) => {
                 if (result.isConfirmed) {
